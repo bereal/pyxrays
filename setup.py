5be2b440b8b6002aa227b46a566e858b328d7f4e
bereal <alexander.berezhnoy@gmail.com>
2012-02-12 12:12:14 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+#from Cython.Build import  cythonize
+
+setup(
+    name = "Pyxrays",
+    packages = ["pyxrays"],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension('pyxrays.cHandler',
+                             ['pyxrays/cHandler.pyx'],
+                             extra_compile_args = ['-O3', '-Wall'])]
+)
